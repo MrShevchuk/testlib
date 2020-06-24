c043d9b43a5355a2fab1ace87a3beff61f6c8268
Maxim Shiryaev <maksim155555@gmail.com>
2020-06-24 19:36:49 +0300

    Maven proj - 1

--- src/main/java/org/brainacad/Main.java
+++ src/main/java/org/brainacad/Main.java
@@ -1,6 +1,14 @@
 package org.brainacad;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Brainacad4 on 24.06.2020.
@@ -12,8 +20,24 @@ public class Main {
         List<String> fruits = Lists.newArrayList("banana", "papaya", "apple", "kiwi");
         fruits.forEach(System.out::println);
 
-        List<String> reverseFruits = List.reverse (fruits);
+        Set<Integer> testSet = Sets.newLinkedHashSet();
+        Set<Integer> testSetNative = new
+
+
+        List<String> reverseFruits = Lists.reverse(fruits);
         reverseFruits.forEach(System.out::println);
+
+        Multimap<String, String> map = ArrayListMultimap.create();
+        map.put("key", "first");
+        map.put("key", "second");
+        System.out.println(map);
+
+
+        Map<String, List<String> oldMap =  new HashMap<>();
+        if (oldMap.containsKey("Key"))
+
+
+
     }
 
 }
